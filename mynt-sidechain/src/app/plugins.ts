0e88dc6ce27057ab9ed127adcfc213949c07ac4a
frankfka <jiafrank98@gmail.com>
2021-06-29 08:05:36 -0700

    Add dashboard plugin

--- mynt-sidechain/src/app/plugins.ts
+++ mynt-sidechain/src/app/plugins.ts
@@ -1,7 +1,9 @@
+import { DashboardPlugin } from '@liskhq/lisk-framework-dashboard-plugin';
 import { Application, HTTPAPIPlugin } from 'lisk-sdk';
 import { UserTokensApiPlugin } from './plugins/UserTokensApi/UserTokensApiPlugin';
 
 export const registerPlugins = (app: Application): void => {
   app.registerPlugin(UserTokensApiPlugin);
   app.registerPlugin(HTTPAPIPlugin);
+  app.registerPlugin(DashboardPlugin);
 };
